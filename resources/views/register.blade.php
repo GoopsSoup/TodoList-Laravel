bfd5d3a2613cb6ab0defca889a1b3c1b40855f46
GoopsSoup <thoughblud@gmail.com>
2026-02-15 19:28:18 +0700

    updated homepage ui

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,21 +7,43 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body class="p-0 m-0">
+<body>
 
-    <section class="flex items-center justify-center h-screen bg-amber-50">
+    <section class="flex items-center justify-center min-h-screen bg-amber-50">
 
-        <div class="flex relative w-[960px] h-[590px] justify-center items-center overflow-hidden">
-            {{-- Register page --}}
-            <div id="register" class="h-130 flex justify-center m-8 absolute z-9 inset-0 transition-all duration-700 ease-in-out"> 
+        <div class="">
+
+            {{-- Register/Login page --}}
+            <div class="h-130 flex justify-center w-full md:w-[62.5vw] max-w-5xl"> 
                     
-                <div class="bg-red-600 flex w-auto h-full"> 
+                <div class="flex flex-col md:flex-row w-full"> 
 
-                    <div class="bg-emerald-400 w-120 flex justify-center items-center rounded-br-3xl rounded-tr-3xl ">
-                        <img src="/images/thejonkler.jpg" alt="The real jonkler" class="w-full h-full p-2 rounded-tr-4xl rounded-br-4xl">
+                    <div class="relative bg-emerald w-full flex justify-center items-center bg-fuchsia-600">
+                        <img id="imageslider" src="/images/thejonkler.jpg" alt="The real jonkler" 
+                        class="w-full h-full     rounded-tr-4xl rounded-br-4xl z-2 transition-all ease-in-out duration-700">
+
+                        <div class="flex flex-col items-center justify-center absolute">
+
+                            <h1 class="text-3xl relative">Login</h1>
+
+                            <form action="/login" method="POST" class="flex flex-col gap-3 pt-9 w-70">
+                                @csrf
+                                <p class="text-sm">Name</p>
+                                <input name="loginName" type="text" placeholder="username" class="bg-fuchsia-400 border-2 h-10 pl-1 w-full">
+                                <p class="text-sm">Email</p>
+                                <input name="loginEmail" type="text" placeholder="user@gmail.com" class="bg-fuchsia-400 border-2 h-10 pl-1 w-full">
+                                <p class="text-sm">Password</p> 
+                                <input name="loginPassword" type="password" class="bg-fuchsia-400 border-2 h-10 pl-1 w-full">
+                                <div class="flex justify-center items-center">
+                                    <button class="bg-cyan-600 border-2 h-12 w-full">Log in</button>
+                                </div>  
+                                <p class="text-center">Don't have an account? <a href="#" onclick="showRegister()">Register</a></p>
+                            </form>
+
+                        </div>
                     </div>
 
-                    <div class="w-120 flex flex-col items-center justify-center">
+                    <div class="w-full flex flex-col items-center justify-center bg-fuchsia-600">
 
                         <h1 class="text-3xl relative">Register</h1>
 
@@ -44,54 +66,30 @@
                 </div>
 
             </div>
-
-            {{-- Login page --}}
-            <div id="login" class="h-130 flex justify-center m-8 absolute inset-0 transition-all duration-700 ease-in-out">
-
-                <div class="bg-red-600 flex w-auto h-full"> 
-
-                    <div class="w-120 flex flex-col items-center justify-center">
-
-                        <h1 class="text-3xl relative">Login</h1>
-
-                        <form action="/login" method="POST" class="flex flex-col gap-3 pt-9 w-70">
-                            @csrf
-                            <p class="text-sm">Name</p>
-                            <input name="loginName" type="text" placeholder="username" class="bg-fuchsia-400 border-2 h-10 pl-1 w-full">
-                            <p class="text-sm">Email</p>
-                            <input name="loginEmail" type="text" placeholder="user@gmail.com" class="bg-fuchsia-400 border-2 h-10 pl-1 w-full">
-                            <p class="text-sm">Password</p> 
-                            <input name="loginPassword" type="password" class="bg-fuchsia-400 border-2 h-10 pl-1 w-full">
-                            <div class="flex justify-center items-center">
-                                <button class="bg-cyan-600 border-2 h-12 w-full">Log in</button>
-                            </div>
-                            <p class="text-center">Don't have an account? <a href="#" onclick="showRegister()">Register</a></p>
-                        </form>
-
-                    </div>
-
-                    <div class="bg-emerald-400 w-120 flex justify-center items-center rounded-bl-3xl rounded-tl-3xl ">
-                        <img src="/images/thejonkler.jpg" alt="The real jonkler" class="w-full h-full p-2 rounded-tl-4xl rounded-bl-4xl">
-                    </div>
-
-                </div>
-
-            </div>
+            
         </div>
 
     </section>
-
 <script>
     function showLogin() {
-        document.getElementById('register').style.transform = "translateX(-200%)";
-        document.getElementById('login').style.transform = "translateX(0%)";
+        const img = document.getElementById('imageslider');
+
+        img.classList.remove('translate-x-0');
+        img.classList.add('translate-x-full');
+
+        img.classList.remove('rounded-tr-4xl', 'rounded-br-4xl');
+        img.classList.add('rounded-tl-4xl', 'rounded-bl-4xl');
     }
 
     function showRegister() {
-        document.getElementById('register').style.transform = "translateX(0%)";
-        document.getElementById('login').style.transform = "translateX(200%)";
-    } 
-</script>
+        const img = document.getElementById('imageslider');
 
+        img.classList.remove('translate-x-full');
+        img.classList.add('translate-x-0');
+
+        img.classList.remove('rounded-tl-4xl', 'rounded-bl-4xl');
+        img.classList.add('rounded-tr-4xl', 'rounded-br-4xl');
+    }
+</script>
 </body>
 </html>
--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -8,52 +8,67 @@
     <title>Document</title>
 </head>
 <body>
-    
-    <h2>Create a new list</h2>
-    <div>
-        <form action="/create-list" method="POST">
-            @csrf
-            <input name="list" type="text" placeholder="Input list here"
-            {{ auth()->guest() ? 'disabled' : '' }}>
-            <button {{ auth()->guest() ? 'disabled' : '' }}>
-                Save List
-            </button>
-        </form>
-    </div>
+    <header class="flex justify-center items-center h-25 bg-amber-500 pb-3">
+        <h1 class="text-2xl">Create a new list</h1>
+    </header>
+    <section class="relative h-screen w-screen bg-blue-400 min-h-screen max-w-12/12">
 
-    <div style="border: 3px solid black">
-        <h3>All Lists here</h3>
-        @foreach ($posts as $post)
-            <div style="border: 3px solid black">
+        <aside class="absolute bg-emerald-600 w-1/19 h-full">
+            <p>hello</p>
+        </aside>
 
-                <h4>{{$post['list']}}</h4>
+        <main class="h-full w-full bg-blue-600 flex items-center justify-center flex-col">
+            <h3 class="p-5 text-3xl">All lists here</h3>
 
-                {{-- <h4>{{$post['list']}} //by {{$post->user->name}}//</h4> --}}
-                <!--untuk melihat siapa yang upload list tersebut ^-->
+            <div style="border: 3px solid black" class="flex items-center bg-red-300 flex-col h-10/12 w-9/12 p-[3%] gap-1 md:gap-2 lg:gap-3">
+                <div class="flex justify-center bg-amber-300">
+                    <form action="/create-list" method="POST">
+                        @csrf
+                        <input name="list" type="text" placeholder="New list here"
+                        {{ auth()->guest() ? 'disabled' : '' }}>
+                        <button {{ auth()->guest() ? 'disabled' : '' }}>
+                            Save List
+                        </button>
+                    </form>
+                </div>
 
-                <p><a href="/edit-list/{{$post->id}}" {{ auth()->guest() ? 'disabled' : '' }}>Edit</a></p>
-                <form action="/delete-list/{{$post->id}}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button {{ auth()->guest() ? 'disabled' : '' }}>
-                        Delete
-                    </button>
-                </form>
+                @foreach ($posts as $post)
+                    <div style="border: 3px solid rgb(49, 0, 228)" 
+                    class="flex justify-between items-center h-2/12 w-10/12 pr-[1%] pl-[1%] rounded-2xl">
+
+                        <h4 class="">{{$post['list']}}</h4>
+
+                        {{-- <h4>{{$post['list']}} //by {{$post->user->name}}//</h4> --}}
+                        <!--untuk melihat siapa yang upload list tersebut ^-->
+                        <div class="flex justify-between w-1/12">
+                            <p><a href="/edit-list/{{$post->id}}" {{ auth()->guest() ? 'disabled' : '' }}>E</a></p>
+                            <form action="/delete-list/{{$post->id}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button {{ auth()->guest() ? 'disabled' : '' }} class="cursor-pointer">
+                                    D
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
-    </div>
+        </main>
 
-    @auth
-    <form action="/logout" method="POST">
-        @csrf
-        <button>Logout!</button>
-    </form>
-    @else
-    <a href="/register">
-        @csrf
-        <button>Register</button>
-    </a>
-    @endauth
+
+        @auth
+        <form action="/logout" method="POST">
+            @csrf
+            <button>Logout!</button>
+        </form>
+        @else
+        <a href="/register">
+            @csrf
+            <button>Register</button>
+        </a>
+        @endauth
+    </section>
+    
     
     
 </body>
