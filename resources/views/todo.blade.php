41a22411744fb7de8d466146fd52b29fbd315b74
GoopsSoup <thoughblud@gmail.com>
2026-03-01 18:41:10 +0700

    calendar fix

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -146,8 +146,11 @@
                     @endauth
                 </a>
 
-                <a href="#" class="flex items-center gap-2.5 px-3 py-2.5 rounded-lg text-white/35 text-sm transition-colors">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="none" stroke="#ffffff59" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 12l5 5L20 7"/></svg>
+                <a href="/?filter=#" class="flex items-center gap-2.5 px-3 py-2.5 rounded-lg text-white/35 text-sm transition-colors
+                    {{ request('filter') === 'complete' || !request('filter') ? 'bg-[#7c6dfa]/20 text-[#7c6dfa]' : 'text-white/35 hover:text-white/65 hover:bg-white/4' }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                    </svg>
                     Completed
                 </a>
 
@@ -225,17 +228,17 @@
                     >
                     {{-- due date --}}
 
-                    <div class="flex items-center w-[4%] justify-center">
-                    <input
-                     {{ auth()->guest() ? 'disabled' : '' }}
-                     type="text" 
-                     name="dueDate"
-                     id="dueDate"
-                     class="bg-red-200 w-5 opacity-0 absolute"
-                    >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6  ">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
-                    </svg>
+                    <div class="flex items-center w-[4%] justify-center bg-[#616161]">
+                        <input
+                        {{ auth()->guest() ? 'disabled' : '' }}
+                        type="text" 
+                        name="dueDate"
+                        id="dueDate"
+                        class="bg-red-200 w-5 opacity-0 absolute"
+                        >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6  ">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                        </svg>
                     </div>
 
                     <button
